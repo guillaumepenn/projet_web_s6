507e1818cd6d26283c9357b65b68d7dfa628338e
guillaumepenn <squac447@gmail.com>
2026-04-16 12:06:45 +0800

    Début projet

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Je déteste le développement web !');
 });
 
 app.listen(port, () => {

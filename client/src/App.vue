8a60cf4580a736be6b9c93c62d68222bfe601eb9
Datieu91 <mathieu.niveau@efrei.net>
2026-05-03 10:47:10 +0200

    remise du reteur qui marche pas

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,6 @@
     <main>
       <!--The router isn't working, so we've adapted the functions so that they don't use Vue Router-->
       <router-view> </router-view>
-      <Profil/>
     </main>
     <AppFooter/>
 </template>
@@ -11,14 +10,12 @@
 <script>
 import Navbar from './components/NavbarComp.vue'
 import AppFooter from './components/FooterComp.vue'
-import Profil from './views/ProfilView.vue'
 
 export default {
   name: 'App',
   components: {
     Navbar,
     AppFooter,
-    Profil
   }
 }
 </script>
